81239209406b0c3b17571db581856c1c239b7258
D3luX8 <123096715+D3luX8@users.noreply.github.com>
2023-03-14 16:27:33 +0100

    Add files via upload
    bash script that takes three parameters, two strings and a directory name, and substitutes any occurrence of the first string with the second string for any file in the directory, recursively

---
+++ script1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Verifica che i parametri siano stati forniti correttamente
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <string1> <string2> <directory>"
+  exit 1
+fi
+
+# Assegna i parametri alle variabili
+string1=$1
+string2=$2
+directory=$3
+
+# Verifica che la directory esista e sia una directory
+if [ ! -d "$directory" ]; then
+  echo "Directory not found: $directory"
+  exit 1
+fi
+
+# Sostituisce ogni occorrenza di string1 con string2 per ogni file nella directory e nei suoi sottodirectory
+find "$directory" -type f -exec sed -i -r "s/\Q$string1\E/$string2/g" {} +
+
+# Mostra un messaggio di conferma
+echo "Sostituzione completata con successo per la directory: $directory"
+
